5f5a9c3d6e6d8af3589b4f31cd57360a8c87707f
ghianluca <ribeiroghian@gmail.com>
2024-09-02 11:23:16 -0300

    feat(book-info): add book details section

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const scrollingAlert = document.querySelector('.scrolling-alert');
 const originalContent = scrollingAlert.innerHTML;
-const numberOfDuplicates = 500; // Ajuste o número conforme necessário
+const numberOfDuplicates = 500;
 
 scrollingAlert.innerHTML = originalContent.repeat(numberOfDuplicates);
