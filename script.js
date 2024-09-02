61764568730650eed4dc0daab192e02d8c1d1d85
ghianluca <ribeiroghian@gmail.com>
2024-09-02 00:50:43 -0300

    fix: correct alert range and resolve related bugs

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 const scrollingAlert = document.querySelector('.scrolling-alert');
+const originalContent = scrollingAlert.innerHTML;
+const numberOfDuplicates = 500; // Ajuste o número conforme necessário
 
-scrollingAlert.innerHTML += scrollingAlert.innerHTML += scrollingAlert.innerHTML += scrollingAlert.innerHTML += scrollingAlert.innerHTML += scrollingAlert.innerHTML += scrollingAlert.innerHTML += scrollingAlert.innerHTML += scrollingAlert.innerHTML += scrollingAlert.innerHTML += scrollingAlert.innerHTML += scrollingAlert.innerHTML += scrollingAlert.innerHTML += scrollingAlert.innerHTML += scrollingAlert.innerHTML += scrollingAlert.innerHTML += scrollingAlert.innerHTML += scrollingAlert.innerHTML;
+scrollingAlert.innerHTML = originalContent.repeat(numberOfDuplicates);
