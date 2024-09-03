7af3c66b6fb0c5ae2c2991399c50b79c175ca632
ghianluca <ribeiroghian@gmail.com>
2024-09-03 10:48:40 -0300

    feat(quantity-books): add quantity books details section

--- scripts/script.js
+++ scripts/script.js
@@ -3,3 +3,15 @@ const originalContent = scrollingAlert.innerHTML;
 const numberOfDuplicates = 500;
 
 scrollingAlert.innerHTML = originalContent.repeat(numberOfDuplicates);
+
+function alterarQuantidade(valor) {
+    const quantidadeElement = document.getElementById('quantity');
+    let quantidadeAtual = parseInt(quantidadeElement.textContent);
+    quantidadeAtual += valor;
+
+    if (quantidadeAtual < 0) {
+        quantidadeAtual = 0;
+    }
+
+    quantidadeElement.textContent = quantidadeAtual;
+}
